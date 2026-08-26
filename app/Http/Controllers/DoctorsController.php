7f12174012e6093598a6b41b1cb535a02615e15f
gita <gitaaasaputri407@gmail.com>
2026-08-26 11:43:41 +0700

    Update Fitur-Doctor

---
+++ app/Http/Controllers/DoctorsController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Doctor;
+use App\Models\doctors;
+use Illuminate\Http\Request;
+
+class DoctorsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $doctors = Doctor::paginate(10);
+        return view('pages.doctors.index', compact('doctors'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('pages.doctors.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(doctors $doctors)
+    {
+        $doctor = Doctor::findOrFail($id);
+        return view ('pages.doctors.show', compact('doctor'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(doctors $doctors)
+    {
+        $doctor = Doctor::findOrFail($id);
+        return view('pages.doctors.edit', compact('doctor'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, doctors $doctors)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(doctors $doctors)
+    {
+        $doctor = Doctor::findOrFail($id);
+        $doctor->delete();
+
+        return redirect()->route('doctors.index')->with('success', 'Delete successfully for ID: ' . $id);
+    }
+}
