b825053032e5bba7e008471554f5f9c54bc532a4
felmi <felmiwulandari02@gmail.com>
2026-08-26 13:58:21 +0700

    Add Fitur-Auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes([
+    'register' => false,
+    'reset' => false,
+    'verify' => false,
+    'confirm' => false,
+]);
+
+Route::group([
+    'prefix' => 'admin',
+    'as' => 'admin',
+    'middleware' => 'auth',
+],  function () {
+    
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
